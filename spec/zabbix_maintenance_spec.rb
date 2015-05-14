7a4b342cd690d1ef863acd549155a407f4b08c24
Marek Skrobacki <skrobul@skrobul.com>
2015-05-14 13:09:50 +0100

    change file names into snake case

--- spec/zabbix_maintenance_spec.rb
+++ spec/zabbix_maintenance_spec.rb
@@ -2,26 +2,27 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require_relative '../lib/zabbix_maintenance'
 
 describe ZabbixMaintenance do
+  let(:zbx) do
+    ZabbixMaintenance.new(
+      'http://192.168.56.2/zabbix/api_jsonrpc.php',
+      'Admin', 'zabbix')
+  end
+
   describe '.new' do
     it 'fails on missing parameters' do
       expect { subject.new }.to raise_error
     end
 
     it 'authenticates with correct credentials', :vcr do
-      zbx = ZabbixMaintenance.new('http://192.168.56.2/zabbix/api_jsonrpc.php',
-                                  'Admin',
-                                  'zabbix')
-      expect(zbx).to be_a ZabbixMaintenance
-      expect(zbx.authenticated?).to eq(true)
+      lzbx = ZabbixMaintenance.new('http://192.168.56.2/zabbix/api_jsonrpc.php',
+                                   'Admin',
+                                   'zabbix')
+      expect(lzbx).to be_a ZabbixMaintenance
+      expect(lzbx.authenticated?).to eq(true)
     end
   end
 
   describe '.create' do
-    let(:zbx) do
-      ZabbixMaintenance.new(
-        'http://192.168.56.2/zabbix/api_jsonrpc.php',
-        'Admin', 'zabbix')
-    end
     context 'with valid parameters' do
       it 'creates maintenance period: with correct groupid', :vcr do
         res = zbx.create([2], period: 3200)
@@ -33,5 +34,9 @@ describe ZabbixMaintenance do
   end
 
   describe '.delete' do
+    it 'removes the maintenance', :vcr do
+      zbx.create([2])
+      expect { zbx.delete }.to change { zbx.exists? }.from(true).to(false)
+    end
   end
 end
